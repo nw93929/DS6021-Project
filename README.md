df7553ed9abb433327b3e2f7bc437a12fa2d3311
William Brannock <wbrannock@vt.edu>
2025-11-16 15:54:42 -0500

    added dashboard file skelton

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Final Project for DS6021 Predictive Modeling @UVA School of Data Science MSDS Program. <br>
 Authors: Nathan Wan, Garret Knapp, Will Brannock, Joseph Kaminetz, Faizan Khan
 
+## Dashboard Info
+Dashboard files are located in the `dashboard/` directory. To run the dashboard, navigate to that directory and run:
+```streamlit run Main.py
+```
+Please do not put any non dashboard files in that directory.
+
---
+++ dashboard/pages/2_LinearRegression.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import time
+import numpy as np
+
+st.set_page_config(page_title="Linear Regression", page_icon="📈")
+
+st.markdown("# Linear Regression")
+st.sidebar.header("Linear Regression")
+st.write(
+    """This shows an example linear regression plot using seaborn"""
+)
+
+# create regression chart using seaborn and display it
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+# load example dataset
+df = sns.load_dataset("tips")
+# create regression plot
+plt.figure(figsize=(10,6))
+sns.regplot(x="total_bill", y="tip", data=df)
+plt.title("Linear Regression of Tip vs Total Bill")
+plt.xlabel("Total Bill")
+plt.ylabel("Tip")
+st.pyplot(plt)
